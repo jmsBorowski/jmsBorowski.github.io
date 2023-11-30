1cff982b682e34bd7dc1b43b6479e4110ef576a8
Jaiden Borowski <jmsborowski03@gmail.com>
2023-11-29 20:33:20 -0500

    basic visual set up

---
+++ code.js
@@ -0,0 +1,6 @@
+// Your JavaScript code for the game logic will go here
+// For now, let's add a simple console log for testing
+document.getElementById('start-btn').addEventListener('click', function() {
+    console.log('Game started!');
+  });
+  
