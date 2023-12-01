765f5a2e6a62d1c5e792388eb397a2785eef5165
Jaiden Borowski <jmsborowski03@gmail.com>
2023-12-01 17:37:13 -0500

    Update code.js

--- code.js
+++ code.js
@@ -22,10 +22,16 @@ function nextSequence() {
     userClickedPattern = [];
     level++;
     updateLevelTitle("Level " + level);
-  
+    
+    // add color to game sequence 
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
     // Iterate over the gamePattern array with a delay between each flash
-    for (let i = 0; i < level; i++) {
+    for (let i = 0; i < gamePattern.length; i++) {
       const currentColor = gamePattern[i];
+      playSound(currentColor);
       setTimeout(() => {
         flashButton(currentColor);
       }, i * 1000); // Adjust the delay (1000 milliseconds = 1 second)
